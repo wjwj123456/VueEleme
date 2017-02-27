02056192b7baac0e349bcdbc597c19cbec9d8af1
wjwj123456 <85736332@qq.com>
2017-02-27 17:30:15 +0800

    wj

--- src/main.js
+++ src/main.js
@@ -1,7 +1,40 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import vueTap from 'v-tap'
+import Vuex from 'vuex'
+
+Vue.use(vueTap)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    // 添加的商品元素
+    addCartEl: {},
+    mutations: {
+        increment(state) {
+            state.count++
+        }
+    }
+})
+const router = new VueRouter({
+    routes: [{
+
+    }],
+})
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+    router,
+    store,
+    template: '<App>',
+    components: {
+        App
+    },
+    data: {
+        eventHub: new Vue()
+    }
+}).$mount('#app')
+
